426f056fd671b672b2fbd43f33b8ac6d6545965d
unknown <flytzen@NOFLSP3.neworbit.local>
2015-03-08 18:40:40 +0000

    Two different F# implementations

---
+++ FSharp1.fsx
@@ -0,0 +1,4 @@
+﻿Seq.unfold (fun (l,r) -> if (l < 1) then None else Some((l,r), (l/2,r*2))) (18,23)
+|> Seq.filter (fun x -> (fst x) % 2 = 1)
+|> Seq.map (fun (l,r) -> r)
+|> Seq.sum 
---
+++ FSharp2.fsx
@@ -0,0 +1,3 @@
+﻿Seq.unfold (fun (l,r) -> if (l < 1) then None else Some((l,r), (l/2,r*2))) (18,23)
+|> Seq.fold(fun total (l,r) -> if (l % 2 = 1) then total + r else total) 0
+;;
